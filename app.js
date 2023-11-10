8c7288b865af189d80d4443886138eeee4046cda
Belia Ardianti <114715788+Beliaardianti@users.noreply.github.com>
2023-11-10 20:49:40 +0700

    update app

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const { Sequelize } = require('sequelize');  
+const adminRouter = require('./routes/adminRouter');
+const customerRouter = require('./routes/customerRouter');
+const supplierRouter = require('./routes/supplierRouter');
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+app.use('/api', adminRouter);
 
-app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+require('dotenv').config();
 
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_DEVELOPMENT,
+  host: process.env.DB_HOST,
+});
+
+// Menyinkronkan model dengan database
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database connected and models synchronized.');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use('/api', customerRouter);
+app.use('/api', supplierRouter);
+
+
+
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Aplikasi berjalan pada port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
--- models/admin.js
+++ models/admin.js
@@ -1,29 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class admin extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Admin extends Model {
     static associate(models) {
-      // define association here
-      admin.hasMany(models.transaction, {
-        foreignKey: 'id_admin'
+      Admin.hasMany(models.Transaction, {
+        foreignKey: "id_admin",
       });
     }
   }
-  admin.init({
-    name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    no_handphone: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'admin',
-  });
-  return admin;
+  Admin.init(
+    {
+      name: DataTypes.STRING,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      no_handphone: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Admin",
+    }
+  );
+  return Admin;
 };

8a318df75c02a014f08f8a35d596d2f1c85d56bc
Belia Ardianti <114715788+Beliaardianti@users.noreply.github.com>
2023-11-13 19:21:35 +0700

    categoryRouter.js

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,73 +1,12 @@
-const { Category } = require('../models/category'); 
+const express = require('express');
+const router = express.Router();
+const categoryController = require('../controllers/category');
 
 
-exports.getAllCategories = async (req, res, next) => {
-    try {
-        const categories = await Category.findAll();
-        res.json(categories);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error' });
-    }
-};
+router.get('/categories', categoryController.getAllCategories);
+router.post('/categories', categoryController.createCategory);
+router.get('/categories/:id', categoryController.getCategoryById);
+router.put('/categories/:id', categoryController.updateCategory);
+router.delete('/categories/:id', categoryController.deleteCategory);
 
-
-exports.getCategoryById = async (req, res, next) => {
-    try {
-        const categoryId = req.params.id;
-        const category = await Category.findByPk(categoryId);
-
-        if (!category) {
-            res.status(404).json({ message: 'Category not found' });
-        } else {
-            res.json(category);
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error' });
-    }
-};
-
-
-exports.createCategory = async (req, res, next) => {
-    try {
-        const newCategory = await Category.create(req.body);
-        res.status(201).json(newCategory);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error' });
-    }
-};
-
-
-exports.updateCategory = async (req, res, next) => {
-    try {
-        const categoryId = req.params.id;
-        const updatedCategory = await Category.update(req.body, { where: { id_category: categoryId } });
-
-        if (updatedCategory[0] === 0) {
-            res.status(404).json({ message: 'Category not found' });
-        } else {
-            res.json({ message: 'Category updated successfully' });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error' });
-    }
-};
-
-exports.deleteCategory = async (req, res, next) => {
-    try {
-        const categoryId = req.params.id;
-        const deletedCategory = await Category.destroy({ where: { id_category: categoryId } });
-
-        if (deletedCategory === 0) {
-            res.status(404).json({ message: 'Category not found' });
-        } else {
-            res.json({ message: 'Category deleted successfully' });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error' });
-    }
-};
+module.exports = router;

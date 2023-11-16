172826a61acb8d8d2ac2e3ed92d87c4537e2bc3f
Merliana Tri S <110303066+merlianats@users.noreply.github.com>
2023-11-16 21:15:48 +0700

    Update inventory.js

--- models/inventory.js
+++ models/inventory.js
@@ -1,34 +1,28 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class inventory extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class Inventory extends Model {
     static associate(models) {
-      // define association here
-      inventory.belongsTo(models.category, {
+      Inventory.belongsTo(models.Category, {
         foreignKey: 'id_category'
       });
-      inventory.hasMany(models.transaction, {
+      Inventory.hasMany(models.Transaction, {
         foreignKey: 'id_inventory'
       });
     }
   }
-  inventory.init({
-    id_category: DataTypes.INTEGER,
-    id_supplier: DataTypes.INTEGER,
-    id_customer: DataTypes.INTEGER,
-    id_admin: DataTypes.INTEGER,
+  Inventory.init({
+    id_category: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Category",
+        key: "id",
+      },
     product_name: DataTypes.STRING,
     stock: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'inventory',
+    modelName: 'Inventory',
   });
-  return inventory;
+  return Inventory;
 };

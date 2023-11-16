d7d73273b4dd994108196135bb28d919dae1cda3
Merliana Tri S <110303066+merlianats@users.noreply.github.com>
2023-11-16 21:14:01 +0700

    Update supplier.js

--- models/supplier.js
+++ models/supplier.js
@@ -1,27 +1,21 @@
 'use strict';
 const { Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class supplier extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class Supplier extends Model {
     static associate(models) {
-      // define association here
-      supplier.hasMany(models.transaction, {
+      Supplier.hasMany(models.Transaction, {
         foreignKey: 'id_supplier'
       });
     }
   }
-  supplier.init({
+  Supplier.init({
     supplier_name: DataTypes.STRING,
     email: DataTypes.STRING,
     no_handphone: DataTypes.STRING,
     address: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'supplier',
+    modelName: 'Supplier',
   });
-  return supplier;
+  return Supplier;
 };

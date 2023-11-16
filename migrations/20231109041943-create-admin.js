4f821de4a1af3ef47c04d59fd85276549263490c
Merliana Tri S <110303066+merlianats@users.noreply.github.com>
2023-11-16 21:01:48 +0700

    Update 20231109041943-create-admin.js

--- migrations/20231109041943-create-admin.js
+++ migrations/20231109041943-create-admin.js
@@ -2,8 +2,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('admins', {
-      id_admin: {
+    await queryInterface.createTable('Admins', {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
@@ -12,7 +12,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
-      username: {
+      email: {
         type: Sequelize.STRING
       },
       password: {
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('admins');
+    await queryInterface.dropTable('Admins');
   }
 };

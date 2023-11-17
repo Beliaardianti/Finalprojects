f12a1b26d77deeff02bd5b40ce5ae4fbf8841624
Agasi2912 <144379945+Agasi2912@users.noreply.github.com>
2023-11-17 10:33:11 +0700

    Update 20231109075334-create-customer.js

--- seeders/20231109075334-create-customer.js
+++ seeders/20231109075334-create-customer.js
@@ -1,29 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    const addCustomer = [
-      {
-        customer_name: "Terasa",
-        email: "terasabeauty@gmail.com",
-        no_handphone:"085246789341",
-        address: "Jalan Pedurungan Kidul 32, Semarang",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        customer_name: "Larissa",
-        email: "larissaskin@gmail.com",
-        no_handphone:"081356789390",
-        address: "Jalan Dr Sudarso 2, Semarang",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]
-    return queryInterface.bulkInsert('customers', addCustomer, {})
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Customers",
+      [
+        {
+          name: "Terasa",
+          email: "terasabeauty@gmail.com",
+          no_handphone: "085246789341",
+          address: "Jalan Pedurungan Kidul 32, Semarang",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Larissa",
+          email: "larissaskin@gmail.com",
+          no_handphone: "081356789390",
+          address: "Jalan Dr Sudarso 2, Semarang",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('customers', null, {});
-  }
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Customers", null, {});
+  },
 };

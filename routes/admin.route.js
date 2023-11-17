047e77366441167e098c4dbbf7cdb25c4a3737a6
Belia Ardianti <114715788+Beliaardianti@users.noreply.github.com>
2023-11-17 15:56:31 +0700

    Update admin.route.js

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,6 +6,7 @@ router.get("/admins", adminController.getAllAdmins);
 router.get("/admins/:id", adminController.getAdminById);
 router.post("/admins", adminController.createAdmin);
 router.put("/admins/:id", adminController.updateAdmin);
+router.post('/admins/login', adminController.loginAdmin);
 router.delete("/admins/:id", adminController.deleteAdmin);
 
 module.exports = router;

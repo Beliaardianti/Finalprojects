0ce8e3f12108209583d4796172d01fcb7ee971b2
Agasi2912 <144379945+Agasi2912@users.noreply.github.com>
2023-11-17 10:34:50 +0700

    Update 20231109075314-create-supplier.js

--- seeders/20231109075314-create-supplier.js
+++ seeders/20231109075314-create-supplier.js
@@ -1,29 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    const addSupplier = [
-      {
-        supplier_name: "Scarlet Official",
-        email: "scarletofficial@gmail.com",
-        no_handphone:"082256789341",
-        address: "Jalan Putri Emas 22, Jakarta",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        supplier_name: "Implora Store",
-        email: "implorastore@gmail.com",
-        no_handphone:"081356789390",
-        address: "Jalan Jendral Sudirman 7, Jakarta",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]
-    return queryInterface.bulkInsert('suppliers', addSupplier, {})
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Suppliers",
+      [
+        {
+          name: "Scarlet Official",
+          email: "scarletofficial@gmail.com",
+          no_handphone: "082256789341",
+          address: "Jalan Putri Emas 22, Jakarta",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Implora Store",
+          email: "implorastore@gmail.com",
+          no_handphone: "081356789390",
+          address: "Jalan Jendral Sudirman 7, Jakarta",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('suppliers', null, {});
-  }
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Suppliers", null, {});
+  },
 };

6ee748ec7e0ef00ce9ae3138ef80b0f237bfacf1
Agasi2912 <144379945+Agasi2912@users.noreply.github.com>
2023-11-17 10:34:20 +0700

    Update 20231109082430-create-transaction.js

--- seeders/20231109082430-create-transaction.js
+++ seeders/20231109082430-create-transaction.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,15 +11,45 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    await queryInterface.bulkInsert(
+      "Transactions",
+      [
+        {
+          id_inventory: 1,
+          id_admin: 1,
+          id_supplier: 1,
+          id_customer: 1,
+          qty: 10,
+          date: new Date(),
+          in_out: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id_inventory: 2,
+          id_admin: 2,
+          id_supplier: 2,
+          id_customer: 2,
+          qty: 15,
+          date: new Date(),
+          in_out: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        // Tambahkan data lainnya sesuai kebutuhan
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    await queryInterface.bulkDelete("Transactions", null, {});
+  },
 };

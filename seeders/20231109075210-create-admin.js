d3b60a75e9178125781019bb49b34ef7ecf82564
Agasi2912 <144379945+Agasi2912@users.noreply.github.com>
2023-11-17 10:31:19 +0700

    Update 20231109075210-create-admin.js

--- seeders/20231109075210-create-admin.js
+++ seeders/20231109075210-create-admin.js
@@ -1,30 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    const addAdmin = [
-      {
-        name: "Deni Caknan",
-        username: "P4ris222",
-        password: "45678900",
-        no_handphone:"083456789341",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "Naura Sati",
-        username: "L0nd0n34",
-        password: "45679283",
-        no_handphone:"089876789212",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]
-    return queryInterface.bulkInsert('admins', addAdmin, {})
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Admins",
+      [
+        {
+          name: "Deni Caknan",
+          username: "P4ris222",
+          password: "45678900",
+          no_handphone: "083456789341",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Naura Sati",
+          username: "L0nd0n34",
+          password: "45679283",
+          no_handphone: "089876789212",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('admins', null, {});
-  }
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Admins", null, {});
+  },
 };

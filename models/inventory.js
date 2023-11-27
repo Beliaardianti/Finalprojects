f28d9425507a3cc6467f46027a675a5a9c74a6e5
SabrinaPutriAulia <21081010048@student.upnjatim.ac.id>
2023-11-27 09:50:21 +0700

    Update inventory.js

--- models/inventory.js
+++ models/inventory.js
@@ -1,28 +1,32 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Inventory extends Model {
     static associate(models) {
       Inventory.belongsTo(models.Category, {
-        foreignKey: 'id_category'
+        foreignKey: "id_category",
       });
       Inventory.hasMany(models.Transaction, {
-        foreignKey: 'id_inventory'
+        foreignKey: "id_inventory",
       });
     }
   }
-  Inventory.init({
-    id_category: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Category",
-        key: "id",
+  Inventory.init(
+    {
+      id_category: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Category",
+          key: "id",
+        },
       },
-    inventory_name: DataTypes.STRING,
-    stock: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Inventory',
-  });
+      inventory_name: DataTypes.STRING,
+      stock: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Inventory",
+    }
+  );
   return Inventory;
 };
